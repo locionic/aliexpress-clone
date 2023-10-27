24e8d1da7c6093a486014eaa17097307629e848c
Truong Hong Loc <hongloc2206@gmail.com>
2023-10-27 16:55:43 +0700

    push

---
+++ components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+      <div class="md:flex gap-4 justify-between w-full">
+        <div clss="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            Help
+          </div>
+          <div class="text-[#999999] text-sm">
+            Help Center, Disputes & ReportingObserver, Buyer ProcessingInstruction, Report IPR 
+          </div>
+        </div>
+        <div clss="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            AliExpress Multi-Language Sites
+          </div>
+          <div class="text-[#999999] text-sm">
+            Russian, Portuguese, Spanish, French, German, Italian, Dutch, Turkish, Japanese, Korean
+          </div>
+        </div>
+      </div>
+
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div clss="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            Browser by Category
+          </div>
+          <div class="text-[#999999] text-sm">
+            All Popular, Product, Promotion, Low Price, Great Value, Reviews, Blog, Seller Portal, BLACK FRIDAY, AliExpress
+          </div>
+        </div>
+        <div clss="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            Alibaba Group
+          </div>
+          <div class="text-[#999999] text-sm">
+            Alibaba Group website, AliExpress, Alibama, Alipay, Fliggy, 
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+      Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use - 
+    </div>
+  </div>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        <div v-if="products" v-for="product in products" :key="product">
+          {{ product }}
+          <ProductComponent :product="product" />
+        </div>
+      </div>
+      
+    </div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from '../layouts/MainLayout.vue';
+const products = [
+  { id: 0, price: 9999, title: "title 1", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+  { id: 1, price: 9999, title: "title 2", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+  { id: 2, price: 9999, title: "title 3", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+  { id: 3, price: 9999, title: "title 4", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+  { id: 4, price: 9999, title: "title 5", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+  { id: 5, price: 9999, title: "title 6", description: "this is a description", url: "https://picsum.photos/id/7/800/800" },
+]
+
+</script>
